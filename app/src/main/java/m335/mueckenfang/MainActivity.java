a844d358a6667840f7ad4a7cf6e4f2a599944546
OrdoRefactorum <106595075+OrdoRefactorum@users.noreply.github.com>
2023-05-23 15:56:09 +0200

    Implemented highscore logic
    so a user can view highscore at the start of the application as well as edit the entry once a new highscore has been achieved.

--- app/src/main/java/m335/mueckenfang/MainActivity.java
+++ app/src/main/java/m335/mueckenfang/MainActivity.java
@@ -2,15 +2,26 @@ package m335.mueckenfang;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ConstraintLayout highscoreAdderContainer, highscoreDisplayContainer;
+    EditText tbxHighscorer;
+    TextView txvHighscorer;
+    Button btnSpeichern, btnStart;
+
+    int punkte = 0;
 
     private final int GAME_REQUESTCODE = 335;
 
@@ -19,14 +30,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnStart = findViewById(R.id.btnStart);
+        highscoreAdderContainer = findViewById(R.id.highscoreAdderContainer);
+        tbxHighscorer = findViewById(R.id.tbxHighscorer);
+        btnSpeichern = findViewById(R.id.btnSpeichern);
+        btnSpeichern.setOnClickListener(this);
+
+        highscoreDisplayContainer = findViewById(R.id.highscoreDisplayContainer);
+        txvHighscorer = findViewById(R.id.txvHighscorer);
+
+        btnStart = findViewById(R.id.btnStart);
         btnStart.setOnClickListener(this);
+
+        highscoreLaden();
     }
 
     @Override
     public void onClick(View view) {
-        Intent gameIntent = new Intent(this, GameActivity.class);
-        startActivityForResult(gameIntent, GAME_REQUESTCODE);
+        if (view == btnStart) {
+            Intent gameIntent = new Intent(this, GameActivity.class);
+            startActivityForResult(gameIntent, GAME_REQUESTCODE);
+        } else if (view == btnSpeichern) {
+            String name = tbxHighscorer.getText().toString();
+            highscoreSpeichern(name, punkte);
+            displayHighscore(name, punkte);
+            highscoreAdderContainer.setVisibility(View.GONE);
+            btnStart.setVisibility(View.VISIBLE);
+        } else {
+            Log.e("M335", "Unbekannter ClickEvent versender");
+        }
+    }
+
+    private void highscoreSpeichern(String name, int punkte) {
+        SharedPreferences sharedPreferences = getSharedPreferences("HIGHSCORE", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("Spieler", name);
+        editor.putInt("Punkte", punkte);
+        editor.apply();
+    }
+
+    private void highscoreLaden() {
+        SharedPreferences sharedPreferences = getSharedPreferences("HIGHSCORE", MODE_PRIVATE);
+        String name = sharedPreferences.getString("Spieler", "");
+        int punkte = sharedPreferences.getInt("Punkte", 0);
+
+        if (!name.equals("") && punkte > 0) {
+            displayHighscore(name, punkte);
+        }
+    }
+
+    private void displayHighscore(String name, int punkte) {
+        String highscore = name + ": " + punkte;
+        highscoreDisplayContainer.setVisibility(View.VISIBLE);
+        txvHighscorer.setText(highscore);
     }
 
     @Override
@@ -34,8 +89,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == GAME_REQUESTCODE && resultCode == Activity.RESULT_OK) {
-            int punkte = data.getIntExtra("punkte", 0);
-            Log.d("M335", "Der Kandidat hat " + punkte + " Punkte erreicht!");
+            punkte = data.getIntExtra("punkte", 0);
+            highscoreAdderContainer.setVisibility(View.VISIBLE);
+            highscoreDisplayContainer.setVisibility(View.GONE);
+            btnStart.setVisibility(View.GONE);
         }
     }
 }
